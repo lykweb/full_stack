44b4534907bfd6318c1cb04b75caa80043dfc133
lyk <1052927018@qq.com>
2021-10-18 01:11:44 +0800

    ts

---
+++ ts/ts-study/dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 1] = "Down";
+    Direction[Direction["Left"] = 2] = "Left";
+    Direction[Direction["Right"] = 3] = "Right";
+})(Direction || (Direction = {}));
+console.log(Direction.Up === 0);
+console.log(Direction.Down === 1);
+console.log(Direction.Left === 2);
+console.log(Direction.Right === 3);
+//# sourceMappingURL=index.js.map
---
+++ ts/ts-study/src/index.ts
@@ -0,0 +1,11 @@
+enum Direction {
+  Up,
+  Down,
+  Left,
+  Right
+}
+
+console.log(Direction.Up === 0); // true
+console.log(Direction.Down === 1); // true
+console.log(Direction.Left === 2); // true
+console.log(Direction.Right === 3); // true
