a1cf2ab863bae47b329b31fd4188b413130ee869
lyk <1052927018@qq.com>
2023-05-11 07:01:09 +0800

    111

--- dataServe/StudentBehaviorMap/StudentBehaviorMap.js
+++ dataServe/StudentBehaviorMap/StudentBehaviorMap.js
@@ -7,7 +7,7 @@ export default {
         y: '0',
         r: 61,
         outerHeight: 22,
-        widthGap: 44,
+        widthGap: 16,
         heightGap: 40,
         outerPath: '',
         textObject: {
@@ -33,18 +33,28 @@ export default {
             nodePath: '',
             children: [
               {
+                x: '0',
+                y: '0',
                 name: '语文：112分'
               },
               {
+                x: '0',
+                y: '0',
                 name: '数学：98分'
               },
               {
+                x: '0',
+                y: '0',
                 name: '英语：119分'
               },
               {
+                x: '0',
+                y: '0',
                 name: '物理：92分'
               },
               {
+                x: '0',
+                y: '0',
                 name: '化学：82分'
               }
             ]
@@ -64,6 +74,8 @@ export default {
             nodePath: '',
             children: [
               {
+                x: '0',
+                y: '0',
                 name: '建筑装饰工程技术'
               }
             ]
@@ -83,6 +95,8 @@ export default {
             nodePath: '',
             children: [
               {
+                x: '0',
+                y: '0',
                 name: '建筑艺术学院'
               }
             ]
@@ -102,19 +116,189 @@ export default {
             height: 32,
             children: [
               {
+                x: '0',
+                y: '0',
                 name: '装饰21-2'
               }
             ]
           }
         ]
-      }
+      },
+      studentStatusItem: {
+        name: '学籍异动',
+        list: [
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          }
+        ]
+      },
+      secClassroomItem: {
+        name: '第二课堂',
+        list: [
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          }
+        ]
+      },
+      achievementItem: {
+        name: '课堂成绩',
+        list: [
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          }
+        ]
+      },
+      signInItem: {
+        name: '上课签到',
+        list: [
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          }
+        ]
+      },
+      booksItem: {
+        name: '图书借阅',
+        list: [
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          },
+          {
+            name: '2019-09-18 转专业 工程造价学院>建筑设备学院'
+          }
+        ]
+      },
+      progressWidth: 0,
+      progressHeight: 0,
+      pNodeR: 0,
+      pNodeLeft: 0,
+      pNodeTop: 0,
+      orbit: 0,
+      studentStatusR: 0,
+      progressItemW: 0,
+      studentStatusLeft: 0,
+      studentStatusTop: 0,
+      secClassroomR: 0,
+      secClassroomLeft: 0,
+      secClassroomTop: 0,
+      achievementR: 0,
+      achievementLeft: 0,
+      achievementTop: 0,
+      signInR: 0,
+      signInLeft: 0,
+      signInTop: 0,
+      booksR: 0,
+      booksLeft: 0,
+      booksop: 0
     }
   },
-  mounted () {},
+  mounted () {
+    this.progressHandle()
+    window.onresize = () => this.progressHandle()
+  },
   created () {
     this.getParentNodeOuterPath()
   },
   methods: {
+    progressHandle () {
+      const progressDom = document.querySelector('.behavior-progress-content')
+      console.log(progressDom.clientWidth)
+      this.progressWidth = progressDom.clientWidth
+      this.progressHeight = this.progressWidth * 0.6057
+      this.pNodeR = this.progressWidth * 0.2171
+      this.pNodeLeft = this.progressWidth / 2 - this.pNodeR / 2
+      this.orbit = this.progressWidth * 0.2857
+      this.progressItemW = this.progressWidth * 0.1657
+      this.studentStatusR = this.progressWidth * 0.0571
+      this.studentStatusLeft = this.progressWidth * 0.0228
+      this.studentStatusTop = this.progressWidth * 0.1743 - 12
+      this.secClassroomR = this.progressWidth * 0.0914
+      this.secClassroomLeft = this.progressWidth * 0.2057
+      this.secClassroomTop = this.progressWidth * 0.2342 - 12
+      this.achievementR = this.progressWidth * 0.1029
+      this.achievementLeft = this.progressWidth * 0.4314 - 16
+      this.achievementTop = this.progressWidth * 0.2714 - 30
+      this.signInR = this.progressWidth * 0.0914
+      this.signInLeft = this.progressWidth * 0.6686 - 50
+      this.signInTop = this.progressWidth * 0.2342 - 20
+      this.booksR = this.progressWidth * 0.0571
+      this.booksLeft = this.progressWidth * 0.8171 - 40
+      this.booksTop = this.progressWidth * 0.1743
+    },
+    moreHandle (item) {
+      console.log(item)
+    },
     getParentNodeOuterPath () {
       const x = this.newStudentNode.r
       const y = 2 * this.newStudentNode.r - this.newStudentNode.outerHeight
@@ -131,12 +315,17 @@ export default {
         childNode.textObject.y = V + 4
         this.newStudentNode.outerPath += `V ${V} H ${H} M ${x} ${V}`
         childNode.nodePath = `M ${H} ${V} Q ${H} ${topV} ${H + cr} ${topV} H ${rLH} Q ${rH} ${topV} ${rH} ${V} Q ${rH} ${bottomV} ${rLH} ${bottomV} H ${H + cr} Q ${H} ${bottomV} ${H} ${V}`
-        V = V + childNode.height + childNode.children.length * this.newStudentNode.heightGap
+        V = V + childNode.height + childNode.children.length * this.newStudentNode.heightGap + this.newStudentNode.heightGap / 2
         if (childNode.children) {
           const nodeH = H + childNode.width / 2
-          childNode.outerPath = `M ${nodeH} ${bottomV}`
+          const { heightGap, widthGap } = childNode
+          let nodeV = bottomV
+          childNode.outerPath = `M ${nodeH} ${nodeV}`
           childNode.children.forEach(item => {
-            childNode.outerPath += `V ${bottomV + item.heightGap} M ${nodeH} ${bottomV + 2 * item.heightGap} V ${bottomV + 3 * item.heightGap} M ${nodeH} ${bottomV + 4 * item.heightGap} V ${bottomV + 5 * item.heightGap} M ${nodeH} ${bottomV + 6 * item.heightGap} V ${bottomV + 7 * item.heightGap} M ${nodeH} ${bottomV + 8 * item.heightGap} H ${H + item.widthGap}`
+            childNode.outerPath += ` V ${nodeV + heightGap} M ${nodeH} ${nodeV + 2 * heightGap} V ${nodeV + 3 * heightGap} M ${nodeH} ${nodeV + 4 * heightGap} V ${nodeV + 5 * heightGap} M ${nodeH} ${nodeV + 6 * heightGap} V ${nodeV + 7 * heightGap} M ${nodeH} ${nodeV + 8 * heightGap} H ${nodeH + widthGap} M ${nodeH} ${nodeV + 8 * heightGap}`
+            nodeV += this.newStudentNode.heightGap
+            item.x = nodeH + 4
+            item.y = nodeV - 4
           })
         }
       })
--- dataServe/StudentBehaviorMap/StudentBehaviorMap.vue
+++ dataServe/StudentBehaviorMap/StudentBehaviorMap.vue
@@ -23,6 +23,7 @@
                 <path :d="childNode.nodePath" stroke="transparent" fill="rgba(64,98,255, 1)" />
                 <text font-size="15" fill="white" :x="childNode.textObject.x" :y="childNode.textObject.y">{{childNode.textObject.text}}</text>
                 <path :d="childNode.outerPath" fill="transparent" stroke="rgba(64,98,255, 0.5)" />
+                <text v-for="subNode in childNode.children" :key="subNode.name" font-size="14" fill="#252f47" :x="subNode.x" :y="subNode.y">{{subNode.name}}</text>
               </g>
             </g>
           </svg>
@@ -31,10 +32,101 @@
       <div class="behavior-progress">
         <div class="behavior-progress-item">
           <div class="behavior-title behavior-progress-title">2019-2020</div>
+          <div :style="{height: progressHeight + 'px'}" class="behavior-content behavior-progress-content">
+            <div class="progress-bgs">
+              <div :style="{height: orbit + 'px'}" class="progress-orbit"></div>
+            </div>
+            <div class="parent-node" :style="{width: pNodeR + 'px', height: pNodeR + 'px', left: pNodeLeft + 'px', top: pNodeTop + 'px'}">
+            </div>
+            <div :style="{width: pNodeR + 'px', height: pNodeR + 'px', left: pNodeLeft + 'px', top: pNodeTop + 'px'}" class="parent-node-text">
+              <div :style="{marginTop: (pNodeR/2 - 30) + 'px'}">学习行为</div>
+              <div class="parent-node-text-bottom">LEARNING BEHAVIOR</div>
+            </div>
+            <!-- 学籍异动 -->
+            <div :style="{width: progressItemW + 'px', left: studentStatusLeft + 'px', top: studentStatusTop + 'px'}" class="student-status">
+              <div :style="{width: studentStatusR + 'px', height: studentStatusR + 'px', marginLeft: (progressItemW/2-studentStatusR/2) + 'px'}" class="student-status-bg"></div>
+              <div class="info-title">{{studentStatusItem.name}}</div>
+              <div v-for="(item, index) in studentStatusItem.list" :key="`${item.name}_${index}}`" class="info-item">
+                <div v-if="index < 5">
+                  <span class="blue-dot"></span>
+                  <span >{{item.name}}</span>
+                </div>
+                <div v-else>
+                  <span class="blue-dot"></span>
+                  <span v-on:click="moreHandle(item)">更多...</span>
+                </div>
+              </div>
+            </div>
+            <!-- 第二课堂 -->
+            <div :style="{width: progressItemW + 'px', left: secClassroomLeft + 'px', top: secClassroomTop + 'px'}" class="student-status">
+              <div :style="{width: secClassroomR + 'px', height: secClassroomR + 'px', marginLeft: (progressItemW/2-secClassroomR/2) + 'px'}" class="student-status-bg"></div>
+              <div class="info-title">{{studentStatusItem.name}}</div>
+              <div v-for="(item, index) in secClassroomItem.list" :key="`${item.name}_${index}}`" class="info-item">
+                <div v-if="index < 5">
+                  <span class="blue-dot"></span>
+                  <span >{{item.name}}</span>
+                </div>
+                <div v-else>
+                  <span class="blue-dot"></span>
+                  <span v-on:click="moreHandle(item)">更多...</span>
+                </div>
+              </div>
+            </div>
+            <!-- 课堂成绩 -->
+            <div :style="{width: progressItemW + 'px', left: achievementLeft + 'px', top: achievementTop + 'px'}" class="student-status">
+              <div :style="{width: achievementR + 'px', height: achievementR + 'px', marginLeft: (progressItemW/2-achievementR/2) + 'px'}" class="student-status-bg"></div>
+              <div class="info-title">{{achievementItem.name}}</div>
+              <div v-for="(item, index) in achievementItem.list" :key="`${item.name}_${index}}`" class="info-item">
+                <div v-if="index < 5">
+                  <span class="blue-dot"></span>
+                  <span >{{item.name}}</span>
+                </div>
+                <div v-else>
+                  <span class="blue-dot"></span>
+                  <span v-on:click="moreHandle(item)">更多...</span>
+                </div>
+              </div>
+            </div>
+            <!-- 上课签到 -->
+            <div :style="{width: progressItemW + 'px', left: signInLeft + 'px', top: signInTop + 'px'}" class="student-status">
+              <div :style="{width: signInR + 'px', height: signInR + 'px', marginLeft: (progressItemW/2-signInR/2) + 'px'}" class="student-status-bg"></div>
+              <div class="info-title">{{signInItem.name}}</div>
+              <div v-for="(item, index) in signInItem.list" :key="`${item.name}_${index}}`" class="info-item">
+                <div v-if="index < 5">
+                  <span class="blue-dot"></span>
+                  <span >{{item.name}}</span>
+                </div>
+                <div v-else>
+                  <span class="blue-dot"></span>
+                  <span v-on:click="moreHandle(item)">更多...</span>
+                </div>
+              </div>
+            </div>
+            <!-- 图书借阅 -->
+            <div :style="{width: progressItemW + 'px', left: booksLeft + 'px', top: secClassroomTop + 'px'}" class="student-status">
+              <div :style="{width: booksR + 'px', height: booksR + 'px', marginLeft: (progressItemW/2-booksR/2) + 'px'}" class="student-status-bg"></div>
+              <div class="info-title">{{booksItem.name}}</div>
+              <div v-for="(item, index) in booksItem.list" :key="`${item.name}_${index}}`" class="info-item">
+                <div v-if="index < 5">
+                  <span class="blue-dot"></span>
+                  <span >{{item.name}}</span>
+                </div>
+                <div v-else>
+                  <span class="blue-dot"></span>
+                  <span v-on:click="moreHandle(item)">更多...</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div class="behavior-end">
         <div class="behavior-title behavior-end-title">毕业后方向</div>
+        <div class="behavior-content behavior-end-content">
+          <div class="behavior-end-text text-job">就业</div>
+          <div class="behavior-end-text text-school">升学</div>
+          <div class="behavior-end-text text-abroad">出国</div>
+        </div>
       </div>
     </div>
   </div>
